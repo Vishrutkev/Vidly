40a83a8bc178de3d69a9d8851f74046121a59853
vishrutkev <kevadiyavishrutd@gmail.com>
2024-06-23 20:12:00 -0600

    deploying the application to Heroku

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const winston = require("winston");
 const { PORT } = require("./config/constants");
 const app = express();
+
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/logging")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const server = app.listen(PORT, () =>
   winston.info(`Listening on port ${PORT}... `)
---
+++ startup/prod.js
@@ -0,0 +1,9 @@
+// All the middleware for the production env
+
+const helmet = require("helmet");
+const compression = require("compression");
+
+module.exports = function (app) {
+  app.use(helmet());
+  app.use(compression());
+};
